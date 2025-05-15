559dcadb437801baffea7757df62f2b197b32d5a
Leonardo Furtado <leonfoliveira@gmail.com>
2025-05-14 21:27:15 -0300

    update corsConfig

--- judge-api/src/main/kotlin/io/leonfoliveira/judge/api/config/CorsConfig.kt
+++ judge-api/src/main/kotlin/io/leonfoliveira/judge/api/config/CorsConfig.kt
@@ -1,14 +1,18 @@
 package io.leonfoliveira.judge.api.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class CorsConfig : WebMvcConfigurer {
+class CorsConfig(
+    @Value("\${server.cors.allowed-origins}")
+    val allowedOrigins: String,
+) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOrigins(allowedOrigins)
             .allowedMethods("*")
     }
 }
