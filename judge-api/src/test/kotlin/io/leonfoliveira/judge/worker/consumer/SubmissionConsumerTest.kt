ebcf7773f3ab19459399a7425822538a35d32695
Leonardo Furtado <leonfoliveira@gmail.com>
2025-05-15 21:18:31 -0300

    add SubmissionConsumerTest

---
+++ judge-api/src/test/kotlin/io/leonfoliveira/judge/worker/consumer/SubmissionConsumerTest.kt
@@ -0,0 +1,24 @@
+package io.leonfoliveira.judge.worker.consumer
+
+import io.kotest.core.spec.style.FunSpec
+import io.leonfoliveira.judge.core.domain.entity.SubmissionMockFactory
+import io.leonfoliveira.judge.core.service.submission.RunSubmissionService
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+
+class SubmissionConsumerTest : FunSpec({
+    val runSubmissionService = mockk<RunSubmissionService>()
+
+    val sut = SubmissionConsumer(runSubmissionService)
+
+    test("should call runSubmissionService with the correct id") {
+        val id = 1
+        every { runSubmissionService.run(id) }
+            .returns(SubmissionMockFactory.build())
+
+        sut.receiveMessage(id.toString())
+
+        verify { runSubmissionService.run(id) }
+    }
+})
