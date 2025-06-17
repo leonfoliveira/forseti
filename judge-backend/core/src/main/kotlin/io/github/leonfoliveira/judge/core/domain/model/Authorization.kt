462556fed1f27e3f36013448c5235762694e2aa0
Leonardo Furtado <leonfoliveira@gmail.com>
2025-06-17 19:47:47 -0300

    fixes

--- judge-backend/core/src/main/kotlin/io/github/leonfoliveira/judge/core/domain/model/Authorization.kt
+++ judge-backend/core/src/main/kotlin/io/github/leonfoliveira/judge/core/domain/model/Authorization.kt
@@ -1,6 +1,9 @@
 package io.github.leonfoliveira.judge.core.domain.model
 
+import java.time.OffsetDateTime
+
 data class Authorization(
     val member: AuthorizationMember,
     val accessToken: String,
+    val expiresAt: OffsetDateTime,
 )
