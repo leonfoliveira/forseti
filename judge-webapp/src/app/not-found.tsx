9a366f7009e523d44796a6c628918e71f4df19c2
Leonardo Furtado <leonfoliveira@gmail.com>
2025-07-14 19:16:26 -0300

    feat: rename ErrorPage component to NotFoundPage for clarity

--- judge-webapp/src/app/not-found.tsx
+++ judge-webapp/src/app/not-found.tsx
@@ -3,7 +3,7 @@
 import { useTranslations } from "next-intl";
 import { ErrorPageTemplate } from "@/app/_component/error-page-template";
 
-export default function ErrorPage() {
+export default function NotFoundPage() {
   const t = useTranslations("not-found");
 
   return <ErrorPageTemplate code={404} description={t("description")} />;
