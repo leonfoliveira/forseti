531d1519ba91a18f1803176792c9e8158156c733
Leonardo Furtado <leonfoliveira@gmail.com>
2025-07-23 22:16:29 -0300

    test: add test for contests layout and page

--- judge-webapp/src/app/contests/[slug]/page.tsx
+++ judge-webapp/src/app/contests/[slug]/page.tsx
@@ -3,18 +3,20 @@
 import { redirect } from "next/navigation";
 import { routes } from "@/config/routes";
 import { useContestMetadata } from "@/app/contests/[slug]/_component/context/contest-metadata-context";
-import { ContestMemberType } from "@/core/domain/enumerate/ContestMemberType";
+import { useAuthorization } from "@/app/_component/context/authorization-context";
+import { MemberType } from "@/core/domain/enumerate/MemberType";
 
 /**
  * ContestPage component that redirects users based on their member type.
  */
 export default function ContestPage() {
+  const { authorization } = useAuthorization();
   const contest = useContestMetadata();
 
-  switch (contest.loggedMemberType) {
-    case ContestMemberType.CONTESTANT:
+  switch (authorization?.member.type) {
+    case MemberType.CONTESTANT:
       return redirect(routes.CONTEST_CONTESTANT(contest.slug));
-    case ContestMemberType.JURY:
+    case MemberType.JURY:
       return redirect(routes.CONTEST_JURY(contest.slug));
     default:
       return redirect(routes.CONTEST_GUEST(contest.slug));
---
+++ judge-webapp/test/app/contests/[slug]/layout.test.tsx
@@ -0,0 +1,28 @@
+import { act, render, screen } from "@testing-library/react";
+import ContestLayout from "@/app/contests/[slug]/layout";
+
+jest.mock(
+  "@/app/contests/[slug]/_component/context/contest-metadata-context",
+  () => ({
+    ContestMetadataProvider: ({ children, slug }: any) => (
+      <div>
+        <span data-testid="slug">{slug}</span>
+        {children}
+      </div>
+    ),
+  }),
+);
+
+describe("ContestLayout", () => {
+  it("renders children within ContestMetadataProvider", async () => {
+    const { params } = { params: Promise.resolve({ slug: "test-slug" }) };
+    const children = <div data-testid="child">Child Component</div>;
+
+    await act(async () => {
+      render(<ContestLayout params={params}>{children}</ContestLayout>);
+    });
+
+    expect(screen.getByTestId("slug")).toHaveTextContent("test-slug");
+    expect(screen.getByTestId("child")).toBeInTheDocument();
+  });
+});
