2db1dee0ce3925ae8fe8bf39bf7621b3eda346a9
Leonardo Furtado <leonfoliveira@gmail.com>
2025-09-09 20:22:04 -0300

    Fix build script to create directories under production path for migrations and sandboxes

--- docker/build.sh
+++ docker/build.sh
@@ -6,10 +6,10 @@ docker build -t judge-webapp ../judge-webapp
 docker build -t judge-autoscaler ../judge-autoscaler
 (cd ../judge-cli && make build)
 
-mkdir -p volumes/migrations
+mkdir -p ./production/volumes/migrations
 cp -r ../judge-backend/common/src/main/resources/migration/* ./production/volumes/migrations/
 
-mkdir -p sandboxes
+mkdir -p ./production/sandboxes
 cp -r ../judge-backend/autojudge/src/main/resources/sandboxes/* ./production/sandboxes/
 
 cp ../judge-cli/dist/judge ./production/judge
