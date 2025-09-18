2fa2ce74949f5ad3d223c166a3f673e676a02ee7
Leonardo Furtado <leonfoliveira@gmail.com>
2025-09-18 20:18:39 -0300

    feat: add Prometheus metrics configuration for MinIO and RabbitMQ, enable scheduling in application

--- judge-backend/autojudge/src/main/kotlin/io/github/leonfoliveira/judge/autojudge/Application.kt
+++ judge-backend/autojudge/src/main/kotlin/io/github/leonfoliveira/judge/autojudge/Application.kt
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.scheduling.annotation.EnableScheduling
 import java.util.TimeZone
 
 @SpringBootApplication(
@@ -30,6 +31,7 @@ import java.util.TimeZone
     ],
 )
 @EnableRabbit
+@EnableScheduling
 class Application
 
 fun main(args: Array<String>) {
