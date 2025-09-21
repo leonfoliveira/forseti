20f2470e4be4ec828fb1d320b75157e784f20bf2
Leonardo Furtado <leonfoliveira@gmail.com>
2025-09-21 11:24:56 -0300

    fix local env

--- judge-webapp/next.config.ts
+++ judge-webapp/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["https://judge.app"],
   experimental: {
     authInterrupts: true,
   },
