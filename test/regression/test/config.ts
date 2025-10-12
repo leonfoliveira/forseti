20da63fd9ff4c1c6528c378c712737f72338715c
Leonardo Furtado <leonfoliveira@gmail.com>
2025-10-12 12:14:43 -0300

    fixes

--- test/regression/test/config.ts
+++ test/regression/test/config.ts
@@ -4,6 +4,6 @@ export const config = {
   CLI_PATH:
     process.env.CLI_PATH ||
     path.resolve(__dirname, "../../../applications/cli/dist/forseti"),
-  ROOT_PASSWORD: process.env.ROOT_PASSWORD || "forseti8",
+  ROOT_PASSWORD: process.env.ROOT_PASSWORD || "forseti",
   API_URL: process.env.API_URL || "http://localhost:8080",
 };
