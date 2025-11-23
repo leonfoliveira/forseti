52d5830837b0ed93d9571cf7024ca440a3a739c4
Leonardo Furtado <leonfoliveira@gmail.com>
2025-11-23 20:44:48 -0300

    update cli

--- applications/cli/cli/util/api_adapter.py
+++ applications/cli/cli/util/api_adapter.py
@@ -19,71 +19,83 @@ VERIFY_SSL = False
 
 class ApiAdapter:
     SERVICE_NAME = "forseti-cli"
+
     SESSION_ID_KEYRING_KEY = "session_id"
     SESSION_ID_COOKIE = "session_id"
+
+    CSRF_TOKEN_KEYRING_KEY = "csrf_token"
+    CSRF_TOKEN_COOKIE = "csrf_token"
+    CSRF_TOKEN_HEADER = "x-csrf-token"
 
     def __init__(self, api_url: str = None):
         self.api_url = api_url or DEFAULT_API_URL
         self.input_adapter = InputAdapter()
 
     def get(self, path: str, **kwargs) -> Union[dict, list]:
-        session_id = self._authenticate()
+        session_id, csrf_token = self._authenticate()
         response = requests.get(
             f"{self.api_url}{path}",
             **kwargs,
             verify=VERIFY_SSL,
             cookies={self.SESSION_ID_COOKIE: session_id},
+            headers={self.CSRF_TOKEN_HEADER: csrf_token},
         )
         if response.status_code != 200:
             raise click.ClickException(response.text)
         return response.json()
 
     def post(self, path: str, json=None, **kwargs) -> Union[dict, list]:
-        session_id = self._authenticate()
+        session_id, csrf_token = self._authenticate()
         response = requests.post(
             f"{self.api_url}{path}",
             json=json,
             **kwargs,
             verify=VERIFY_SSL,
             cookies={self.SESSION_ID_COOKIE: session_id},
+            headers={self.CSRF_TOKEN_HEADER: csrf_token},
         )
         if response.status_code != 200:
             raise click.ClickException(response.text)
         return response.json()
 
     def put(self, path: str, json=None, **kwargs) -> Union[dict, list]:
-        session_id = self._authenticate()
+        session_id, csrf_token = self._authenticate()
         response = requests.put(
             f"{self.api_url}{path}",
             json=json,
             **kwargs,
             verify=VERIFY_SSL,
             cookies={self.SESSION_ID_COOKIE: session_id},
+            headers={self.CSRF_TOKEN_HEADER: csrf_token},
         )
         if response.status_code != 200:
             raise click.ClickException(response.text)
         return response.json()
 
     def delete(self, path: str, **kwargs) -> None:
-        session_id = self._authenticate()
+        session_id, csrf_token = self._authenticate()
         response = requests.delete(
             f"{self.api_url}{path}",
             **kwargs,
             verify=VERIFY_SSL,
             cookies={self.SESSION_ID_COOKIE: session_id},
+            headers={self.CSRF_TOKEN_HEADER: csrf_token},
         )
         if response.status_code != 204:
             raise click.ClickException(response.text)
 
     def _authenticate(self):
-        if session_id := self._get_cached_session_id():
+        if (
+            (session_id := self._get_cached_value(self.SESSION_ID_KEYRING_KEY))
+            and (csrf_token := self._get_cached_value(self.CSRF_TOKEN_KEYRING_KEY))
+        ):
             response = requests.get(
                 f"{self.api_url}/v1/session/me",
                 verify=VERIFY_SSL,
                 cookies={self.SESSION_ID_COOKIE: session_id},
             )
             if response.status_code == 200:
-                return session_id
+                return session_id, csrf_token
 
         password = self.input_adapter.password("Root password: ")
         response = requests.post(
@@ -95,24 +107,26 @@ class ApiAdapter:
             raise click.ClickException(response.text)
         cookies = response.headers["Set-Cookie"]
         session_id = re.search(r"(?<=session_id=)[^;]+", cookies).group(0)
+        csrf_token = re.search(r"(?<=csrf_token=)[^;]+", cookies).group(0)
 
-        self._set_cached_session_id(session_id)
+        self._set_cached_value(self.SESSION_ID_KEYRING_KEY, session_id)
+        self._set_cached_value(self.CSRF_TOKEN_KEYRING_KEY, csrf_token)
 
-        return session_id
+        return session_id, csrf_token
 
-    def _get_cached_session_id(self) -> str:
+    def _get_cached_value(self, key: str) -> str:
         try:
-            return keyring.get_password(self.SERVICE_NAME, self.SESSION_ID_KEYRING_KEY)
+            return keyring.get_password(self.SERVICE_NAME, key)
         except keyring.errors.NoKeyringError:
             click.echo(
-                "Warning: No keyring backend available, session ID will not be cached."
+                f"Warning: No keyring backend available, {key} will not be cached."
             )
             return None
 
-    def _set_cached_session_id(self, session_id: str) -> None:
+    def _set_cached_value(self, key: str, value: str) -> None:
         try:
             keyring.set_password(
-                self.SERVICE_NAME, self.SESSION_ID_KEYRING_KEY, session_id
+                self.SERVICE_NAME, key, value
             )
         except keyring.errors.NoKeyringError:
             pass
