fea693a64f04fa8ca39d04d922893f885e38ebb9
Leonardo Furtado <leonfoliveira@gmail.com>
2025-11-30 11:32:34 -0300

    remove setKey from storage reader

--- applications/webapp/src/core/port/driving/usecase/storage/StorageReader.ts
+++ applications/webapp/src/core/port/driving/usecase/storage/StorageReader.ts
@@ -1,13 +1,5 @@
 export interface StorageReader {
   /**
-   * Store a value associated with a key.
-   *
-   * @param key The key to associate the value with
-   * @param value The value to store
-   */
-  setKey<TValue>(key: string, value: TValue): void;
-
-  /**
    * Retrieve a value associated with a key.
    *
    * @param key The key whose associated value is to be returned
