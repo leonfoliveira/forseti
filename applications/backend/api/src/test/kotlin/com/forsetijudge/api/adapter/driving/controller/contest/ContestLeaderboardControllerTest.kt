b16c12091baffe28eac7978424319b86871cfb2c
Leonardo Furtado <leonfoliveira@gmail.com>
2026-02-14 20:43:16 -0300

    add endpoint

--- applications/backend/api/src/test/kotlin/com/forsetijudge/api/adapter/driving/controller/contest/ContestLeaderboardControllerTest.kt
+++ applications/backend/api/src/test/kotlin/com/forsetijudge/api/adapter/driving/controller/contest/ContestLeaderboardControllerTest.kt
@@ -2,7 +2,10 @@ package com.forsetijudge.api.adapter.driving.controller.contest
 
 import com.forsetijudge.api.adapter.driving.advice.GlobalExceptionHandler
 import com.forsetijudge.core.config.JacksonConfig
+import com.forsetijudge.core.domain.entity.SessionMockBuilder
+import com.forsetijudge.core.domain.model.RequestContext
 import com.forsetijudge.core.port.driving.usecase.leaderboard.BuildLeaderboardUseCase
+import com.forsetijudge.core.port.driving.usecase.leaderboard.FreezeLeaderboardUseCase
 import com.forsetijudge.core.port.dto.output.LeaderboardOutputDTO
 import com.github.f4b6a3.uuid.UuidCreator
 import com.ninjasquad.springmockk.MockkBean
@@ -10,12 +13,14 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.extensions.spring.SpringExtension
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.put
 
 @WebMvcTest(controllers = [ContestLeaderboardController::class])
 @AutoConfigureMockMvc(addFilters = false)
@@ -23,11 +28,18 @@ import org.springframework.test.web.servlet.get
 class ContestLeaderboardControllerTest(
     @MockkBean(relaxed = true)
     private val findLeaderboardUseCase: BuildLeaderboardUseCase,
+    @MockkBean(relaxed = true)
+    private val freezeLeaderboardUseCase: FreezeLeaderboardUseCase,
     private val webMvc: MockMvc,
 ) : FunSpec({
         extensions(SpringExtension)
 
         val basePath = "/api/v1/contests/{contestId}/leaderboard"
+        val session = SessionMockBuilder.build()
+
+        beforeEach {
+            RequestContext.getContext().session = session
+        }
 
         test("findContestLeaderboardById") {
             val contestId = UuidCreator.getTimeOrderedEpoch()
@@ -41,5 +53,31 @@ class ContestLeaderboardControllerTest(
                     status { isOk() }
                     content { leaderboard }
                 }
+        }
+
+        test("freezeContestLeaderboardById") {
+            val contestId = UuidCreator.getTimeOrderedEpoch()
+
+            webMvc
+                .put("$basePath:freeze", contestId) {
+                    accept = MediaType.APPLICATION_JSON
+                }.andExpect {
+                    status { isOk() }
+                }
+
+            verify { freezeLeaderboardUseCase.freeze(contestId, session.member.id) }
+        }
+
+        test("unfreezeContestLeaderboardById") {
+            val contestId = UuidCreator.getTimeOrderedEpoch()
+
+            webMvc
+                .put("$basePath:unfreeze", contestId) {
+                    accept = MediaType.APPLICATION_JSON
+                }.andExpect {
+                    status { isOk() }
+                }
+
+            verify { freezeLeaderboardUseCase.unfreeze(contestId, session.member.id) }
         }
     })
