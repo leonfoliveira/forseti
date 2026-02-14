5e4b6f3c70751186e90b0c44461d0736a3d17f7e
Leonardo Furtado <leonfoliveira@gmail.com>
2026-02-14 17:55:22 -0300

    implement StompLeaderboardListener

--- applications/webapp/src/infrastructure/adapter/stomp/StompLeaderboardListener.ts
+++ applications/webapp/src/infrastructure/adapter/stomp/StompLeaderboardListener.ts
@@ -1,5 +1,6 @@
 import { LeaderboardListener } from "@/core/port/driven/listener/LeaderboardListener";
 import { ListenerClient } from "@/core/port/driven/listener/ListenerClient";
+import { LeaderboardPartialResponseDTO } from "@/core/port/dto/response/leaderboard/LeaderboardPartialResponseDTO";
 import { LeaderboardResponseDTO } from "@/core/port/dto/response/leaderboard/LeaderboardResponseDTO";
 
 export class StompLeaderboardListener implements LeaderboardListener {
@@ -16,5 +17,24 @@ export class StompLeaderboardListener implements LeaderboardListener {
     cb: (submission: LeaderboardResponseDTO) => void,
   ): Promise<void> {
     await client.subscribe(`/topic/contests/${contestId}/leaderboard`, cb);
+  }
+
+  /**
+   * Subscribe to partial leaderboard updates for a contest.
+   * A partial leaderboard update contains only a member / problem cell.
+   *
+   * @param client The listener client used to subscribe to the partial leaderboard.
+   * @param contestId ID of the contest
+   * @param cb Callback function to handle incoming partial leaderboard updates.
+   */
+  async subscribeForLeaderboardPartial(
+    client: ListenerClient,
+    contestId: string,
+    cb: (leaderboard: LeaderboardPartialResponseDTO) => void,
+  ): Promise<void> {
+    await client.subscribe(
+      `/topic/contests/${contestId}/leaderboard/partial`,
+      cb,
+    );
   }
 }
