b2a2763bf5e0f6da64a812c7503d997dc7f6648b
Leonardo Furtado <leonfoliveira@gmail.com>
2026-03-02 11:20:26 -0300

    feat: add latest image tags for services in docker-compose.yaml and prune unused images in build.sh

--- deployment/build.sh
+++ deployment/build.sh
@@ -19,4 +19,6 @@ cd ../applications/backend/infrastructure/src/main/resources/sandboxes
 docker build -t forseti-sb-cpp17:latest -f cpp17.Dockerfile .
 docker build -t forseti-sb-java21:latest -f java21.Dockerfile .
 docker build -t forseti-sb-python312:latest -f python312.Dockerfile .
+
+docker image prune -f
 cd - > /dev/null
