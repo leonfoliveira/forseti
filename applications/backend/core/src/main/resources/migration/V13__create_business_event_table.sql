3a9153f105436f19aa3a3ba386a9680cdb8b3bd4
Leonardo Furtado <leonfoliveira@gmail.com>
2026-04-04 12:14:32 -0300

    remove unused transaction

--- applications/backend/core/src/main/resources/migration/V13__create_business_event_table.sql
+++
@@ -1,8 +0,0 @@
-create table business_event (
-    id uuid not null primary key,
-    created_at timestamp not null,
-    updated_at timestamp not null,
-    status text not null,
-    type text not null,
-    payload jsonb not null
-);
